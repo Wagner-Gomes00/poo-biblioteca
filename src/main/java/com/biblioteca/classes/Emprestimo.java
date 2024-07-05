a3489e054d54716c0f8218c0586367f88c7c0a19
wagner gomes <wagnergomes2132@gmail.com>
2024-07-05 16:57:01 -0300

    Devolução

--- src/main/java/com/biblioteca/classes/Emprestimo.java
+++ src/main/java/com/biblioteca/classes/Emprestimo.java
@@ -2,4 +2,39 @@ package com.biblioteca.classes;
 
 public class Emprestimo {
 
+    private int emprestimoId;
+    private String dataEmprestimo;
+    private String dataPrevDev;
+
+    // Constructor
+    public Emprestimo(int emprestimoId, String dataEmprestimo, String dataPrevDev) {
+        this.emprestimoId = emprestimoId;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataPrevDev = dataPrevDev;
+    }
+
+    // Getters e Setters
+    public int getEmprestimoId() {
+        return emprestimoId;
+    }
+
+    public void setEmprestimoId(int emprestimoId) {
+        this.emprestimoId = emprestimoId;
+    }
+
+    public String getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(String dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public String getDataPrevDev() {
+        return dataPrevDev;
+    }
+
+    public void setDataPrevDev(String dataPrevDev) {
+        this.dataPrevDev = dataPrevDev;
+    }
 }
