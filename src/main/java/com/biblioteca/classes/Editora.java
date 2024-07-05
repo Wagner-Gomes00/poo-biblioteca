71909e41aa60a6055eae6b564741bca9637f285e
wagner gomes <wagnergomes2132@gmail.com>
2024-07-05 17:32:09 -0300

    adicionando contador nas classes com ID

--- src/main/java/com/biblioteca/classes/Editora.java
+++ src/main/java/com/biblioteca/classes/Editora.java
@@ -1,28 +1,39 @@
 package com.biblioteca.classes;
+
 import java.util.*;
+
 public class Editora {
+    public static int contador = 0;
     private int editoraID;
     private String nome;
 
-    //Constructor
-    public Editora(int editoraID, String nome){
+    // contador de id
+    public Editora(int editoraID) {
+        this.editoraID = editoraID;
+        this.editoraID = contador;
+        contador++;
+    }
+
+    // Constructor
+    public Editora(int editoraID, String nome) {
         this.editoraID = editoraID;
         this.nome = nome;
     }
 
-    //Getters e sett
-    public int geteditoraID(){
+    // Getters e sett
+    public int geteditoraID() {
         return editoraID;
     }
 
-    public void seteditoraID(int editoraID){
+    public void seteditoraID(int editoraID) {
         this.editoraID = editoraID;
     }
 
-    public String getnome(){
+    public String getnome() {
         return nome;
     }
-    public void setnome(String nome){
+
+    public void setnome(String nome) {
         this.nome = nome;
     }
 
--- src/main/java/com/biblioteca/classes/ExemplarEmprestimo.java
+++ src/main/java/com/biblioteca/classes/ExemplarEmprestimo.java
@@ -1,5 +1,23 @@
 package com.biblioteca.classes;
 
 public class ExemplarEmprestimo {
+    public static int contador = 0;
+    private int exemplarEmprestimoId;
 
+       
+    // Construtor/contador
+    public ExemplarEmprestimo(int exemplarEmprestimoId) {
+        this.exemplarEmprestimoId = exemplarEmprestimoId;
+        this.exemplarEmprestimoId = contador;
+        contador++;
+    }
+
+    // Getters e Setters
+    public int getExemplarEmprestimoId() {
+        return exemplarEmprestimoId;
+    }
+
+    public void setExemplarEmprestimoId(int exemplarEmprestimoId) {
+        this.exemplarEmprestimoId = exemplarEmprestimoId;
+    }
 }
