6bc33b20945f06711fe45a62e5f9dcf055ddf841
wagner gomes <wagnergomes2132@gmail.com>
2024-07-05 16:43:41 -0300

    Livro

--- src/main/java/com/biblioteca/classes/Editora.java
+++ src/main/java/com/biblioteca/classes/Editora.java
@@ -1,5 +1,29 @@
 package com.biblioteca.classes;
-
+import java.util.*;
 public class Editora {
+    private int editoraID;
+    private String nome;
+
+    //Constructor
+    public Editora(int editoraID, String nome){
+        this.editoraID = editoraID;
+        this.nome = nome;
+    }
+
+    //Getters e sett
+    public int geteditoraID(){
+        return editoraID;
+    }
+
+    public void seteditoraID(int editoraID){
+        this.editoraID = editoraID;
+    }
+
+    public String getnome(){
+        return nome;
+    }
+    public void setnome(String nome){
+        this.nome = nome;
+    }
 
 }
